67783f026a82e9c9ef01020a569ec1f2138fd268
kokk <kokk7310@gmail.com>
2025-09-27 08:44:21 +0900

    Initial commit

---
+++ src/main/java/com/example/kkproj/KkprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.kkproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkprojApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KkprojApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/kkproj/KkprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kkproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkprojApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
